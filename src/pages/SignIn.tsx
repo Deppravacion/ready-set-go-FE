42fcab920ecf01d90ec99b18eec66cd10bc78df5
Tyson <deppravacion@gmail.com>
2024-10-30 08:08:23 -0500

    add: key to mapped items in dom

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -37,47 +37,47 @@ export const SignIn = () => {
   const fields = [{ name: "email" }, { name: "password" }];
   return (
     <>
-      <div data-theme={userTheme} className='hero min-h-screen '>
-        <div className='hero-content flex-col lg:flex-row-reverse'>
-          <div className='text-center lg:text-left'>
-            <h1 className='text-5xl font-bold'>Login now!</h1>
-            <p className='py-6'>
+      <div data-theme={userTheme} className="hero min-h-screen ">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
               Welcome Back to READY-SET-GO! Please login to your account.
             </p>
           </div>
-          <div className='card shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
-            <form className='card-body' onSubmit={handleSubmit}>
-              <div className='form-control'>
-                <div className='avatar justify-center'>
-                  <div className='w-36 mask mask-squircle'>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body" onSubmit={handleSubmit}>
+              <div className="form-control">
+                <div className="avatar justify-center">
+                  <div className="w-36 mask mask-squircle">
                     <img src={profileImage} />
                   </div>
                 </div>
-                {fields.map((field) => (
-                  <>
-                    <label className='label'>
-                      <span className='label-text'>{field.name}</span>
+                {fields.map((field, i) => (
+                  <div key={i}>
+                    <label className="label">
+                      <span className="label-text">{field.name}</span>
                     </label>
                     <input
                       type={field.name}
                       placeholder={field.name}
-                      className='input input-bordered'
+                      className="input input-bordered"
                       onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
                         setUser({ ...user, [field.name]: e.target.value })
                       }
                       required
                     />
-                  </>
+                  </div>
                 ))}
               </div>
 
-              <div className='form-control mt-6 flex gap-1'>
-                <button type='submit' className='btn btn-info'>
+              <div className="form-control mt-6 flex gap-1">
+                <button type="submit" className="btn btn-info">
                   Login
                 </button>
                 <button
-                  type='button'
-                  className='btn btn-primary'
+                  type="button"
+                  className="btn btn-primary"
                   onClick={() => goToSignUp()}
                 >
                   Sign Up
